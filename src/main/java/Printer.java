265d92637a44c6932d31cfcb9a96cc48d490ec05
DeuterGraves <carolinetgraves@gmail.com>
2018-11-19 19:24:34 +0000

    printer passing check for paper, confirm enough paper and printing tests

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,33 @@
 public class Printer {
+    private int pages;
+
+
+    public Printer(int pages){
+        this.pages = pages;
+    }
+
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int pagesNeeded(int pages, int copies) {
+        return pages * copies;
+    }
+
+    public String confirmPrint(int pages, int copies) {
+        int pagesNeededToPrint = this.pagesNeeded(pages, copies);
+        if (pagesNeededToPrint <= this.pages){
+            return "printing";
+        }
+        return "not enough paper";
+    }
+
+    public int print(int pages, int copies) {
+        int pagesNeededtoPrint = this.pagesNeeded(pages, copies);
+        if (pagesNeededtoPrint <= this.pages) {
+            return this.pages - pagesNeededtoPrint;
+        }
+        return this.pages;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -15,16 +16,32 @@ public class PrinterTest {
     //    Create a Printer class that has a property for number of sheets of paper left.
     @Test
     public void hasPaper(){
-        assertEquals(100, printer.getPaper());
+        assertEquals(100, printer.getPages());
     }
 
     //    Add a method to print that takes in a number of pages to be printed and number of copies to print.
+
     @Test
     public void calculatesPagesNeeded(){
         assertEquals(25, printer.pagesNeeded(5, 5));
     }
 
     //    The print method will only run if the printer has enough paper. If it runs it will reduce the value of the paper left by number of copies * number of pages.
+
+//    Method - returns string 'can print' /can't print - tests if can print and calls print
+    @Test
+    public void confirmPrinterCanPrint(){
+        assertEquals("printing", printer.confirmPrint(5, 5));
+    }
+
+    @Test
+    public void confirmPrinterCannotPrint(){
+        Printer lowPaper = new Printer(20);
+        assertEquals("not enough paper", lowPaper.confirmPrint(5, 5));
+    }
+
+
+//    Prints - decreases paper by pages * copies
     @Test
     public void printerPrints(){
         assertEquals(75, printer.print(5, 5));
@@ -33,18 +50,21 @@ public class PrinterTest {
     @Test
     public void printerDoesntPrint(){
         Printer lowPaper = new Printer(45);
-        assertEquals(45, printer.print(5, 10));
+        assertEquals(45, lowPaper.print(5, 10));
     }
 
+
     //    Create a method to refill the printer paper.
-    @Test
+
+   @Test
     public void refillPrinter(){
         Printer lowPaper = new Printer(45);
         assertEquals(100, printer.refill());
     }
 
+/*
     //    Add a toner volume property to the class.
-    /*
+
     @Test
     public void printerHasToner(){
     assertEquals(100, printer.getToner());
