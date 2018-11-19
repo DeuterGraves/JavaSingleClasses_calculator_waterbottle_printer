f8e0e86cd96b38a240f512aebaa9574d7a300282
DeuterGraves <carolinetgraves@gmail.com>
2018-11-19 17:19:08 +0000

    all waterbottle tests written and passing

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,28 @@
 public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume){
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public int drink() {
+        if (this.volume >= 10) {
+            return this.volume - 10;
+        }
+        return volume;
+    }
+
+    public int empty() {
+        return this.volume - this.volume;
+    }
+
+
+    public int fill() {
+        return this.volume = 100;
+    }
+
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,9 +1,72 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class WaterBottleTest {
 
-//    Water Bottle
-//Create a water bottle class with a volume property.
-//The volume should start at 100.
-//Add a drink function that takes 10 from the volume each time it is called.
-//Create an empty function that brings the volume down to 0.
-//Create a fill function that fills the volume back to 100.
+    private WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+
+    //    test value of volume at start (expect 100)
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+
+//    test value of volume after drinking - each drink decreases by 10
+    @Test
+    public void drinkingDecreasesVolumeBy10(){
+        assertEquals(90, waterBottle.drink());
+    }
+
+//    only drink what remains if volume < 10
+    @Test
+    public void dontDrinkLessThan10(){
+        WaterBottle lowWater = new WaterBottle(5);
+        assertEquals(5, lowWater.drink());
+    }
+
+// shouldn't drink if value is < 0
+    @Test
+    public void DontDrinkIf0(){
+        WaterBottle emptyBottle = new WaterBottle(0);
+        assertEquals(0,emptyBottle.drink());
+    }
+
+//    empty and test value is 0
+    @Test
+    public void emptyWaterBottle(){
+        assertEquals(0, waterBottle.empty());
+    }
+
+
+    @Test
+    public void emptyLessThanFullBottle(){
+//        waterBottle.drink();
+        WaterBottle lowWater = new WaterBottle(5);
+        assertEquals(0, lowWater.empty());
+    }
+
+
+//    fill and test value is 100
+    @Test
+    public void fillWaterBottle(){
+        assertEquals(100, waterBottle.fill());
+    }
+
+
+    @Test
+    public void fillLessFullWaterBottle(){
+        WaterBottle lowWater = new WaterBottle(55);
+        assertEquals(100, lowWater.fill());
+    }
+
+
 }
